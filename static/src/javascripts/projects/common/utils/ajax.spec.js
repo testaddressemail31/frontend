2530f1e9cf552def6031d48f3943332d2aa36cce
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-01 12:47:10 +0000

    Add correct jest mocking

--- static/src/javascripts/projects/common/utils/ajax.spec.js
+++ static/src/javascripts/projects/common/utils/ajax.spec.js
@@ -1,18 +1,25 @@
+import ajax from './ajax';
+import reqwestSpy from 'reqwest'; // This is mocked
+
+jest.mock('reqwest', () => jest.fn(() => ({
+    then: () => {},
+})));
+
+jest.mock('../../../../javascripts-legacy/projects/common/utils/config', () => ({
+    page: {
+        ajaxUrl: 'http://api.nextgen.guardianapps.co.uk',
+    },
+}));
+
+jest.mock('../../../../javascripts-legacy/projects/common/utils/raven', () => ({
+    wrap: () => {},
+}));
+
 describe('AJAX', () => {
-    const ajax = require('common/utils/ajax');
-
-    const reqwest = jest.fn(() => ({
-        then() {},
-    }));
-
-    beforeEach(() => {
-
-    });
-
     it('should proxy calls to reqwest', () => {
         ajax({ url: '/endpoint.json', param: 'value' });
 
-        expect(reqwest).toHaveBeenCalledWith({
+        expect(reqwestSpy).toHaveBeenCalledWith({
             url: 'http://api.nextgen.guardianapps.co.uk/endpoint.json',
             crossOrigin: true,
             param: 'value',
@@ -22,26 +29,26 @@ describe('AJAX', () => {
     it('should not touch a url that is already absolute', () => {
         ajax({ url: 'http://apis.guardian.co.uk/endpoint.json' });
 
-        expect(reqwest).toHaveBeenCalledWith({ url: 'http://apis.guardian.co.uk/endpoint.json' });
+        expect(reqwestSpy).toHaveBeenCalledWith({ url: 'http://apis.guardian.co.uk/endpoint.json' });
     });
 
     it('should not touch a url that is already absolute (https)', () => {
         ajax({ url: 'https://apis.guardian.co.uk/endpoint.json' });
 
-        expect(reqwest).toHaveBeenCalledWith({ url: 'https://apis.guardian.co.uk/endpoint.json' });
+        expect(reqwestSpy).toHaveBeenCalledWith({ url: 'https://apis.guardian.co.uk/endpoint.json' });
     });
 
     it('should not touch a protocol-less url', () => {
         ajax({ url: '//apis.guardian.co.uk/endpoint.json' });
 
-        expect(reqwest).toHaveBeenCalledWith({ url: '//apis.guardian.co.uk/endpoint.json' });
+        expect(reqwestSpy).toHaveBeenCalledWith({ url: '//apis.guardian.co.uk/endpoint.json' });
     });
 
     it('should be able to update host', () => {
         ajax.setHost('http://apis.guardian.co.uk');
         ajax({ url: '/endpoint.json' });
 
-        expect(reqwest).toHaveBeenCalledWith({
+        expect(reqwestSpy).toHaveBeenCalledWith({
             url: 'http://apis.guardian.co.uk/endpoint.json',
             crossOrigin: true,
         });
