66dc6aec3742a05111375a1a694bc088448e8a6a
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-03-02 09:20:18 +0000

    transform projects/common/utils/clamp.js content to es6

--- static/src/javascripts/projects/common/utils/clamp.js
+++ static/src/javascripts/projects/common/utils/clamp.js
@@ -1,13 +1,13 @@
 import bonzo from 'bonzo';
 import bean from 'bean';
 
-var clamp = function(elem, lines, showMore) {
-    var height = elem.clientHeight,
-        lineHeight = getComputedStyle(elem).getPropertyValue('line-height'),
-        maxHeight = (parseInt(lineHeight, 10) + (showMore ? 2 : 0)) * lines,
-        $fade = bonzo(bonzo.create('<span class="clamp-fade"></span>')),
-        $elem = bonzo(elem),
-        $showMore;
+const clamp = (elem, lines, showMore) => {
+    const height = elem.clientHeight;
+    const lineHeight = getComputedStyle(elem).getPropertyValue('line-height');
+    const maxHeight = (parseInt(lineHeight, 10) + (showMore ? 2 : 0)) * lines;
+    const $fade = bonzo(bonzo.create('<span class="clamp-fade"></span>'));
+    const $elem = bonzo(elem);
+    let $showMore;
 
     if (height < maxHeight) {
         return;
@@ -23,7 +23,7 @@ var clamp = function(elem, lines, showMore) {
     if (showMore) {
         $showMore = bonzo(bonzo.create('<span class="clamp-fade__content u-fauxlink" role="button">Read more</span>'));
         $fade.append($showMore);
-        bean.on($showMore[0], 'click', function() {
+        bean.on($showMore[0], 'click', () => {
             $fade.remove();
             $elem.css({
                 maxHeight: 'none',
