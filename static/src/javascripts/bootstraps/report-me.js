dc4e2b3b922e9a966adc4eb8f8c48da8a2e6fc1c
Alex Ware <alex.ware@guardian.co.uk>
2017-03-09 17:06:49 +0000

    hack day changes

--- static/src/javascripts/bootstraps/report-me.js
+++ static/src/javascripts/bootstraps/report-me.js
@@ -1,43 +1,55 @@
-import template from 'lib/template'
-import * as t from 'raw-loader!common/views/report-me.html'
-import * as ty from 'raw-loader!common/views/report-me-tyvm.html'
+import template from 'lib/template';
+import * as t from 'raw-loader!common/views/report-me.html';
+import * as ty from 'raw-loader!common/views/report-me-tyvm.html';
+const endpoint = 'http://localhost:9990/report';
+function report(comment,n){
+    let path = window.location.pathname;
+    fetch(endpoint, {
+        method: 'POST',
+        headers: new Headers({'content-type': 'application/json'}),
+        body: JSON.stringify({
+            article: path,
+            paragraph: n,
+            comment: comment
+        })
+    })
+}
+
 export default {
     init: () => {
-        let form = document.createElement('div');
+        const form = document.createElement('div');
 
-
-        let paras = [... document.querySelectorAll('.js-article__body > p')];
-        paras.map((p)=>{
-            let hide = () => {
-                form.remove()
+        const paras = [...document.querySelectorAll('.js-article__body > p')];
+        paras.map((p, n) => {
+            const hide = () => {
+                form.remove();
             };
-            let show = () => {
-                form.innerHTML =  t.default;
-                let input = form.querySelector('.js-report-me-input');
-                let submit = form.querySelector('.js-report-me-button');
+            const show = () => {
+                form.innerHTML = t.default;
+                const input = form.querySelector('.js-report-me-input');
+                const submit = form.querySelector('.js-report-me-button');
                 p.appendChild(form);
-                submit.addEventListener('click',()=>{
-                    console.log(input.value);
-                    console.log(ty.default)
+                submit.addEventListener('click', () => {
+                    console.log(n);
+                    report(input.value,n);
                     form.innerHTML = ty.default;
                 });
             };
-            let button = document.createElement('div');
+            const button = document.createElement('div');
             button.classList.add('report-me');
             button.appendChild(document.createTextNode('🐙'));
-           p.addEventListener('mouseover',(e)=>{
-               p.appendChild(button);
-           });
-            p.addEventListener('mouseout',(e)=>{
+            p.addEventListener('mouseover', e => {
+                p.appendChild(button);
             });
+            p.addEventListener('mouseout', e => {});
 
-            button.addEventListener('click',(e)=>{
-                if(form.parentNode == p){
-                    hide()
+            button.addEventListener('click', e => {
+                if (form.parentNode == p) {
+                    hide();
                 } else {
-                    show()
+                    show();
                 }
-            })
+            });
         });
-    }
-}
+    },
+};
