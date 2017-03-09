15adfb51454150680ba79e99db769f2e8cf83802
Kate Whalen <whalenka88@gmail.com>
2017-03-09 17:06:31 +0000

    Basic messaging insertion on inactive slots

--- static/src/javascripts-legacy/projects/commercial/modules/close-disabled-slots.js
+++ static/src/javascripts-legacy/projects/commercial/modules/close-disabled-slots.js
@@ -6,6 +6,11 @@ define([
     fastdom
 ) {
     var adSlotSelector = '.js-ad-slot';
+    var awesomeSlotSelector = '.js-awesome-slot';
+
+    var template = "<p>Lots of people love reading us…</p>" +
+    "<p>…but love alone doesn’t keep the lights on</p>" +
+    "<p>The love and support of our readers is vital to securing our future. Our fearless, independent journalism takes a lot of time, hard work and money to produce. And it is increasingly funded by our readers. That’s why we need you to help.</p>";
 
     return {
         init: init
@@ -15,6 +20,7 @@ define([
 
         // Get all ad slots
         var adSlots = qwery(adSlotSelector);
+        var awesomeSlots = qwery(awesomeSlotSelector);
 
         if (!force) {
             // remove the ones which should not be there
@@ -23,12 +29,30 @@ define([
 
         return fastdom.write(function () {
             adSlots.forEach(function (adSlot) {
-                adSlot.parentNode.removeChild(adSlot);
+              adSlot.parentNode.removeChild(adSlot);
             });
+
+            awesomeSlots.forEach(function (awesomeSlot) {
+              if (hasHiddenSlot(awesomeSlot)) {
+                addAwesome(awesomeSlot, template);
+              }
+            })
         });
     }
 
     function shouldDisableAdSlot(adSlot) {
         return window.getComputedStyle(adSlot).display === 'none';
+    }
+
+    function hasHiddenSlot(awesomeSlot) {
+      return window.getComputedStyle(awesomeSlot).height != '0px' && (!awesomeSlot.children[0] || awesomeSlot.children[0].hasAttribute('hidden'));
+    }
+
+    function addAwesome(awesomeSlot, template) {
+      if (hasHiddenSlot(awesomeSlot)) {
+        var span = document.createElement("span");
+        span.innerHTML = template;
+        awesomeSlot.appendChild(span);
+      }
     }
 });
